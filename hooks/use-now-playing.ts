2360fdbb8a0ff2f64f853adb686f8f71e63e614d
root <root@LAPTOP-F5EHCMK8.>
2024-11-09 13:59:23 +0800

    feat: add spotify currently playing song

--- hooks/use-now-playing.ts
+++ hooks/use-now-playing.ts
@@ -6,6 +6,7 @@ import type { SpotifyNowPlayingData } from '@/types/index';
 
 export default function useNowPlaying() {
   const { data } = useSWR<SpotifyNowPlayingData>('/api/spotify', fetcher);
+  console.log('data', data);
 
   return data || { isPlaying: false };
 }
--- servers/spotify.server.ts
+++ servers/spotify.server.ts
@@ -1,5 +1,13 @@
 import { SPOTIFY_TOKEN_API, SPOTIFY_NOW_PLAYING_API } from '@/constants/index';
 
+import dotenv from 'dotenv';
+
+// 尝试加载 .env.local 文件
+const localEnv = dotenv.config({ path: '.env.local' });
+if (localEnv.error) {
+  // 如果 .env.local 文件不存在，加载 .env 文件
+  dotenv.config({ path: '.env' });
+}
 const {
   SPOTIFY_CLIENT_ID: client_id,
   SPOTIFY_CLIENT_SECRET: client_secret,
@@ -25,16 +33,38 @@ async function getAccessToken() {
   return response.json();
 }
 
+// export async function getSpotifyNowPlaying() {
+//   const { access_token } = await getAccessToken();
+
+//   const url = new URL(SPOTIFY_NOW_PLAYING_API);
+
+//   url.searchParams.append('additional_types', 'track,episode');
+
+//   return fetch(url.toString(), {
+//     headers: {
+//       Authorization: `Bearer ${access_token}`,
+//     },
+//   });
+// }
 export async function getSpotifyNowPlaying() {
   const { access_token } = await getAccessToken();
 
   const url = new URL(SPOTIFY_NOW_PLAYING_API);
-
   url.searchParams.append('additional_types', 'track,episode');
 
-  return fetch(url.toString(), {
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
+  try {
+    const response = await fetch(url.toString(), {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const data = await response;
+    console.log('Now Playing Data:', data);
+
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch Now Playing data:', error);
+    return null;
+  }
 }
