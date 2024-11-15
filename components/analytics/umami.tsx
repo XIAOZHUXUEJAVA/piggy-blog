a9288836d0756750c029274b17bb55f5a899e8f9
root <root@LAPTOP-F5EHCMK8.>
2024-11-15 14:33:08 +0800

    test

--- components/analytics/umami.tsx
+++ components/analytics/umami.tsx
@@ -1,12 +1,14 @@
-import Script from 'next/script.js';
+import Script from 'next/script';
 
 interface UmamiAnalyticsProps {
   websiteId?: string;
   src?: string;
 }
 
-export function UmamiAnalytics({ websiteId, src = '/stats/script.js' }: UmamiAnalyticsProps) {
-  // export function UmamiAnalytics({ websiteId, src = './script.js' }: UmamiAnalyticsProps) {
+export function UmamiAnalytics({
+  websiteId,
+  src = '/script.js', // 指向 public/script.js
+}: UmamiAnalyticsProps) {
   if (websiteId) {
     return <Script defer data-website-id={websiteId} src={src} strategy="afterInteractive" />;
   }
