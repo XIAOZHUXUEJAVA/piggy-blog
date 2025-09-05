ec47a45b940e670d58b2e4ac75d8a22954f0cf01
XIAOZHUXUEJAVA <2393751426@qq.com>
2025-09-05 15:46:09 +0800

    styles: remove cursor styles

--- app/Main.tsx
+++ app/Main.tsx
@@ -24,25 +24,25 @@ const MAX_DISPLAY = 5;
 export default function Home({ posts }) {
   const [showScrollButton, setShowScrollButton] = useState(false);
 
-  useEffect(() => {
-    // Initialize canvas animation
-    newCanvas('canvas');
+  // useEffect(() => {
+  //   // Initialize canvas animation
+  //   newCanvas('canvas');
 
-    // Handle scroll events to show or hide the button
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setShowScrollButton(true); // Show the button when scrolled down
-      } else {
-        setShowScrollButton(false); // Hide the button when at the top
-      }
-    };
+  //   // Handle scroll events to show or hide the button
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 100) {
+  //       setShowScrollButton(true); // Show the button when scrolled down
+  //     } else {
+  //       setShowScrollButton(false); // Hide the button when at the top
+  //     }
+  //   };
 
-    // Attach the scroll event listener
-    window.addEventListener('scroll', handleScroll);
+  //   // Attach the scroll event listener
+  //   window.addEventListener('scroll', handleScroll);
 
-    // Clean up the event listener when the component is unmounted
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  //   // Clean up the event listener when the component is unmounted
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, []);
 
   // Scroll to top function
   const scrollToTop = () => {
@@ -57,7 +57,7 @@ export default function Home({ posts }) {
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#8882_1px,transparent_1px),linear-gradient(to_bottom,#8882_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
       </div>
 
-      <canvas id="canvas" className="fixed inset-0 z-[0] h-screen w-screen"></canvas>
+      {/* <canvas id="canvas" className="fixed inset-0 z-[0] h-screen w-screen"></canvas> */}
 
       <Snowfall
         snowflakeCount={60}
